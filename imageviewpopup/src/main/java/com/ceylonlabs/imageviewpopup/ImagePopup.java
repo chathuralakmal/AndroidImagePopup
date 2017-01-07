e2bb3fe48c760e133eb63dc88d6b70a8a1595a53
imchathura@outlook.com <mac@g1thub>
2017-01-07 20:51:45 +0530

    Close Button Added

--- imageviewpopup/src/main/java/com/ceylonlabs/imageviewpopup/ImagePopup.java
+++ imageviewpopup/src/main/java/com/ceylonlabs/imageviewpopup/ImagePopup.java
@@ -1,12 +1,16 @@
 package com.ceylonlabs.imageviewpopup;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -18,75 +22,82 @@ import android.widget.PopupWindow;
  */
 
 public class ImagePopup extends ImageView {
-        private Context context;
-        private PopupWindow popupWindow;
+    private Context context;
+    private PopupWindow popupWindow;
 
-        private int windowHeight = 800;
-        private int windowWidth = 800;
+    private int windowHeight = 800;
+    private int windowWidth = 800;
 
-        private int backgroundColor = Color.parseColor("#FFFFFF");
+    private int windowBackgroundColor = Color.parseColor("#FFFFFF");
 
 
-        public ImagePopup(Context context) {
-            super(context);
-            this.context = context;
+    public ImagePopup(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    public ImagePopup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public int getWindowHeight() {
+        return windowHeight;
+    }
+
+    public void setWindowHeight(int windowHeight) {
+        this.windowHeight = windowHeight;
+    }
+
+    public int getWindowWidth() {
+        return windowWidth;
+    }
+
+    public void setWindowWidth(int windowWidth) {
+        this.windowWidth = windowWidth;
+    }
+
+    public int getWindowBackgroundColor() {
+        return windowBackgroundColor;
+    }
+
+    public void setWindowBackgroundColor(int windowBackgroundColor) {
+        this.windowBackgroundColor = windowBackgroundColor;
+    }
+
+    public void initiatePopup(Drawable drawable){
+        try{
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+            View layout = inflater.inflate(R.layout.popup,(ViewGroup)findViewById(R.id.popup));
+            layout.setBackgroundColor(getWindowBackgroundColor());
+
+            ImageView imageView = (ImageView)layout.findViewById(R.id.imageView);
+            imageView.setImageDrawable(drawable);
+
+            popupWindow = new PopupWindow(layout, getWindowWidth(), getWindowHeight(), true);
+
+            popupWindow.showAtLocation(layout, Gravity.CENTER, 0,0);
+
+            ImageView closeIcon = (ImageView)layout.findViewById(R.id.closeBtn);
+            closeIcon.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    popupWindow.dismiss();
+                }
+            });
+
+            /** Background dim part **/
+            View container = (View) popupWindow.getContentView().getParent();
+            WindowManager wm = (WindowManager) context.getSystemService(context.WINDOW_SERVICE);
+            WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) container.getLayoutParams();
+            layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+            layoutParams.dimAmount = 0.3f;
+            wm.updateViewLayout(container, layoutParams);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-        public ImagePopup(Context context, AttributeSet attrs) {
-            super(context, attrs);
-        }
-
-        public int getWindowHeight() {
-            return windowHeight;
-        }
-
-        public void setWindowHeight(int windowHeight) {
-            this.windowHeight = windowHeight;
-        }
-
-        public int getWindowWidth() {
-            return windowWidth;
-        }
-
-        public void setWindowWidth(int windowWidth) {
-            this.windowWidth = windowWidth;
-        }
-
-        public int getBackgroundColor() {
-            return backgroundColor;
-        }
-
-        @Override
-        public void setBackgroundColor(int backgroundColor) {
-            this.backgroundColor = backgroundColor;
-        }
-
-        public void initiatePopup(Drawable drawable){
-            try{
-                LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-
-                View layout = inflater.inflate(R.layout.popup,(ViewGroup)findViewById(R.id.popup));
-                layout.setBackgroundColor(getBackgroundColor());
-
-                ImageView imageView = (ImageView)layout.findViewById(R.id.imageView);
-                imageView.setImageDrawable(drawable);
-
-                popupWindow = new PopupWindow(layout, getWindowWidth(), getWindowHeight(), true);
-
-                popupWindow.showAtLocation(layout, Gravity.CENTER, 0,0);
-
-                /** Background dim part **/
-                View container = (View) popupWindow.getContentView().getParent();
-                WindowManager wm = (WindowManager) context.getSystemService(context.WINDOW_SERVICE);
-                WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) container.getLayoutParams();
-                layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-                layoutParams.dimAmount = 0.3f;
-                wm.updateViewLayout(container, layoutParams);
-
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+    }
 
 }
